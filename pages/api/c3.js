f3fac90be20d1b6a8a62413d3c7aae51d6ef7b63
Tom McCollough <tom.mccollough@gmail.com>
2023-03-06 16:08:49 -0700

    dbg

--- pages/api/c3.js
+++ pages/api/c3.js
@@ -50,13 +50,16 @@ export default function handler(req, res) {
         console.error(error.message);
         res.status(500).send({ error: 'GET error' });
     });
+    console.log('handler done')
 }
 
-function sendmail(connected, temp) {
+async function sendmail(connected, temp) {
     // using Twilio SendGrid's v3 Node.js Library
     // https://github.com/sendgrid/sendgrid-nodejs
-    const sgMail = require('@sendgrid/mail')
-    sgMail.setApiKey(process.env.SENDGRID_API_PING_SPA2)
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_PING_SPA2);
+    console.log('starting email')
+    console.log('sendgrid key', process.env.SENDGRID_API_PING_SPA2);
     const msg = {
         to: 'tom.mccollough@gmail.com', // Change to your recipient
         from: 'tom@tommccollough.com', // Change to your verified sender
@@ -64,31 +67,28 @@ function sendmail(connected, temp) {
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
     }
-    sgMail
+    await sgMail
         .send(msg)
-        .then(() => {
-            console.log('Email sent')
-        })
-        .catch((error) => {
-            console.error(error);
-            console.log('email did not work');
-        });
-        console.log('email setup done');
+        .then(() => { console.log('Email sent') })
+        .catch((error) => { console.error(error); console.log('email did not work'); });
+    console.log('email function done');
 }
 
-function sendtext(connected, temp) {
+async function sendtext(connected, temp) {
     // Download the helper library from https://www.twilio.com/docs/node/install
     // Find your Account SID and Auth Token at twilio.com/console
     // and set the environment variables. See http://twil.io/secure
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
     const client = require('twilio')(accountSid, authToken);
-
-    client.messages.create({
-            body: `Spa is connected: ${connected}, temp is ${temp}`,
-            from: '+18888207345',
-            to: '+18016478498'
-        })
-        .then(message => console.log(`sms was successful ${message.sid}`));
-    console.log('SMS setup done')
+    console.log('starting sms')
+    console.log('twilio account id', process.env.TWILIO_ACCOUNT_SID);
+    console.log('twilio auth token', process.env.TWILIO_AUTH_TOKEN);
+    await client.messages.create({
+        body: `Spa is connected: ${connected}, temp is ${temp}`,
+        from: '+18888207345',
+        to: '+18016478498'
+    })
+        .then(message => console.log(`sms was successful, message id is ${message.sid}`));
+    console.log('SMS function done');
 }
