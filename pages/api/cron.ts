f9655ba331b437d9d4a73d4750eaff312b04e545
Tom McCollough <tom.mccollough@gmail.com>
2023-03-05 21:43:36 -0700

    my updates

---
+++ pages/api/cron.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  response.status(200).json({
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+    env: process.env.MY_SECRET,
+  });
+}
